78a4bccc5e9fcd878d24624e0255c05f4fbbc41e
yulechka <bubnovaum@gmail.com>
2023-10-29 19:25:30 +0400

    login test added

--- ui/locators/login_page_locators.py
+++ ui/locators/login_page_locators.py
@@ -1,1 +1,9 @@
 from selenium.webdriver.common.by import By
+
+
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.ID, ":r0:")
+    LOGIN_PASSWORD = (By.ID, ":r1:")
+    LOGIN_BUTTON = (By.ID, "login_btn")
+    USER_MENU = (By.ID, "openUserMenu")
