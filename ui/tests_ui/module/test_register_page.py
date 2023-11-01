574a224f2cf1870a7918551df5c4e9b94ffa7832
Victoria Chernova <chernovavi.qa@gmail.com>
2023-11-01 14:18:13 +0600

    addings to the register test

--- ui/tests_ui/module/test_register_page.py
+++ ui/tests_ui/module/test_register_page.py
@@ -10,6 +10,7 @@ from allure_commons.types import AttachmentType
 
 
 from ui.data_ui import RegisterPageData
+from ui.data_ui import PlannerPageData
 from ui.pages.register_page import RegisterPage
 from ui.locators.register_page_locators import RegisterPageLocators
 
@@ -23,7 +24,7 @@ from ui.locators.register_page_locators import RegisterPageLocators
 def test_register_valid_short_ver(browser):
     link = RegisterPageData.REGISTER_PAGE_URL
     page = RegisterPage(browser, link)
-    wait = WebDriverWait(browser, 3)
+    wait = WebDriverWait(browser, 10)
     page.open()
     page.enter_first_name(RegisterPageData.REGISTER_FIRST_NAME_VALID)
     page.enter_last_name(RegisterPageData.REGISTER_LAST_NAME_VALID)
@@ -31,15 +32,16 @@ def test_register_valid_short_ver(browser):
     page.enter_org_name(RegisterPageData.REGISTER_ORGANIZATION_NAME_VALID)
     page.enter_password(RegisterPageData.REGISTER_PASSWORD_VALID)
     page.click_create_org_btn()
+    wait.until(EC.visibility_of_element_located(RegisterPageLocators.REGISTER_SKIP_BTN))
     page.skip_org_detail_form()
 
     with allure.step('the result of the successful registration'):
         allure.attach(browser.get_screenshot_as_png(), name='register_result', attachment_type=AttachmentType.PNG)
 
-    wait.until(EC.url_to_be(RegisterPageData.PLANNER_PAGE_URL))
+    wait.until(EC.url_to_be(PlannerPageData.PLANNER_PAGE_URL))
     fact_result_url = browser.current_url
-    exp_result_url = RegisterPageData.PLANNER_PAGE_URL
-    assert fact_result_url == exp_result_url
+    exp_result_url = PlannerPageData.PLANNER_PAGE_URL
+    assert exp_result_url in fact_result_url
 
 
 
